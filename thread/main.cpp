8b05082ae1a8110870da5cbf144c3cd48a260148
rashid <rassant@mail.ru>
2020-06-09 19:36:45 +0300

    operator()

--- thread/main.cpp
+++ thread/main.cpp
@@ -4,41 +4,15 @@
 #include <thread>
 #include "./resources/function/headers/foo.hpp"
 #include "./resources/class/headers/class.hpp"
-#include "./resources/oops/headers/oops.hpp"
 
 void some(){};
 int main()
 {
-// ---------- создание потока -------------------------------------------
-//
-/*	std::thread t (countSpace);
-	t.detach();
-	countNumber(); */
-// ======================================================================
-
-
 // --------- поток из экземпляра класса operator ()() const -------------
-/*
-	{
 	background_task some;
 	std::thread t(some);
 	t.detach();
 	countSpace();
-	}
-*/
-// ======================================================================
-
-
-// ----------- поток использую лямбда функцию --------------------------
-/*
-	std::thread t ([] {
-			      countSpace();
-			   });
-	t.detach();	
-	countStar();*/
 // =====================================================================
 
-	oops();
-int n =44;
-func i(n);
 }//main
