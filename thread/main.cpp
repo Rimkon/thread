9282c8d4ed9608858bdd4c52eb4f85c7859fe3c4
rashid <rassant@mail.ru>
2020-06-13 00:43:08 +0300

    возврат из функции

--- thread/main.cpp
+++ thread/main.cpp
@@ -2,23 +2,45 @@
 #include <thread>
 #include "./resources/function/headers/foo.hpp"
 
-int main()
+using namespace std;
+/*
+void
+some_function()
+{}
+
+void 
+some_other_function (int)
+{}
+*/
+std::thread f() {
+				return std::thread (countSpace);		// и вернули ее в потоке.
+}; // thread f()
+
+
+std::thread g() {							
+				std::thread t (countNumber,3000);	// передали ее в поток
+				return t;								// вернули ее
+}; // thread g()
+
+/*
+void f (std::thread t);
+
+void g ()
 {
-<<<<<<< HEAD
-	{
-	oops();
-	}
-	int n =44;
-	func i(n);
-countSpace();
-countStar();
-=======
-	std::thread t ([]{
-				countNumber(1500);
-				countStar();
-				}
-			);
-	t.detach();
-	countSpace();
->>>>>>> 0d99404390b9814457ccf64287b04768066d11bb
+	void 
+	some_function();
+
+	f (std::thread     (some_function));
+	   std::thread  t  (some_function);
+	f (std::move (t));
+
+}//void g()
+*/
+
+int main() {
+	std::thread t1 = f();
+	t1.detach();
+
+	std::thread t2 = g();
+    t2.join();
 }//main
