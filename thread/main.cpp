2c9c2d47b76388b14d8c20307706bbd985e440ff
rashid <rassant@mail.ru>
2020-06-13 00:46:19 +0300

    mend

--- thread/main.cpp
+++ thread/main.cpp
@@ -3,39 +3,19 @@
 #include "./resources/function/headers/foo.hpp"
 
 using namespace std;
-/*
-void
-some_function()
-{}
 
-void 
-some_other_function (int)
-{}
-*/
+
+
 std::thread f() {
-				return std::thread (countSpace);		// и вернули ее в потоке.
+		return std::thread (countSpace);	// и вернули ее в потоке.
 }; // thread f()
 
 
 std::thread g() {							
-				std::thread t (countNumber,3000);	// передали ее в поток
+                 std::thread t (countNumber,3000);	// передали ее в поток
 				return t;								// вернули ее
 }; // thread g()
 
-/*
-void f (std::thread t);
-
-void g ()
-{
-	void 
-	some_function();
-
-	f (std::thread     (some_function));
-	   std::thread  t  (some_function);
-	f (std::move (t));
-
-}//void g()
-*/
 
 int main() {
 	std::thread t1 = f();
